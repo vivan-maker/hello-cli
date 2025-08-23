0add58bdfd124d220a96936344eff29ac1e95452
vivan-maker <tiantahilia@gmail.com>
2025-08-23 08:13:36 +0700

    menambahkan logging

--- log_hello_cli/src/log_hello_cli/main.py
+++ log_hello_cli/src/log_hello_cli/main.py
@@ -1,12 +1,19 @@
 import argparse
+import logging
 from log_hello_cli import sapa
-
+logging.basicConfig(filename='log.txt', level=logging.INFO)
 def main():
     parser = argparse.ArgumentParser(description="Program sapaan sederhana.")
     parser.add_argument("--nama", type=str, required=True, help="Nama kamu")
     args = parser.parse_args()
 
-    print(sapa(args.nama))
+    try:
+        pesan = sapa(args.nama)
+        logging.info(f'program dijakankan oleh {args.nama}')
+        print(pesan)
+    except Exception as e:
+        logging.error(f'ERRO : {e}')
+        print('maaf terjadi kesalahan')
 
 if __name__ == "__main__":
     main()
